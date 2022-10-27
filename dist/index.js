0c8b8c10fa1a3950eba521f31d1734d78f96481b
Justin <justintorre75@gmail.com>
2022-10-27 02:30:41 -0700

    added pusher and subscribe support

--- dist/index.js
+++ dist/index.js
@@ -1,116 +1,191 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PromptZero = void 0;
+exports.PromptZero = exports.initPromptZero = exports.DEFAULT_URI = void 0;
 const client_1 = require("@apollo/client");
+const pusher_js_1 = __importDefault(require("pusher-js"));
+exports.DEFAULT_URI = "https://www.promptzero.com/api/graphql";
+async function initPromptZero(apiKey, uri = exports.DEFAULT_URI, realTime = true) {
+    const p = new PromptZero({ apiKey, uri, realTime });
+    await p.init();
+    return p;
+}
+exports.initPromptZero = initPromptZero;
+function apolloToCacheResult(result) {
+    const error = result.error;
+    if (error === undefined) {
+        return {
+            error: null,
+            data: result.data.requestedPrompt,
+        };
+    }
+    else {
+        return {
+            error: error.message,
+            data: null,
+        };
+    }
+}
 class PromptZero {
-    constructor(apiKey, uri = "https://www.promptzero.com/api/graphql") {
+    constructor({ apiKey, uri, realTime }) {
+        this.userId = "";
+        this.updateCache = { statusChanges: {} };
+        this.pusher = new pusher_js_1.default("8499475eff3faa817087", {
+            cluster: "us3",
+        });
+        this.pusher.signin();
         this.client = new client_1.ApolloClient({
             cache: new client_1.InMemoryCache(),
             headers: {
                 authorization: apiKey,
             },
-            uri
+            uri,
+        });
+    }
+    async init() {
+        this.userId = (await this.getUser()).data.user.id;
+        const channel = this.pusher.subscribe(this.userId);
+        channel.bind("status-change", (promptId) => this.getPromptResult(promptId)
+            .then((result) => {
+            this.updateCache.statusChanges[promptId] =
+                apolloToCacheResult(result);
+        })
+            .catch((e) => {
+            this.updateCache.statusChanges[promptId] = {
+                error: e.message,
+                data: null,
+            };
+        }));
+    }
+    async getUser() {
+        return await this.client.query({
+            query: (0, client_1.gql) `
+        query {
+          user {
+            id
+          }
+        }
+      `,
         });
     }
     async getPrompts() {
-        return await this.client
-            .query({
+        return await this.client.query({
             query: (0, client_1.gql) `
-                query {
-                    requestedPrompts(limit: 100, orderBy:{orderBy:CreatedAt, direction: Dsc}) {
-                    status
-                    createdAt
-                    prompt
-                    result {
-                        ... on Result_StableDiffusionV1_4 {
-                        __typename
-                        id
-                        images {
-                            url
-                        }
-                        }
-                    }
-                    }
+        query {
+          requestedPrompts(
+            limit: 100
+            orderBy: { orderBy: CreatedAt, direction: Dsc }
+          ) {
+            status
+            createdAt
+            prompt
+            result {
+              ... on Result_StableDiffusionV1_4 {
+                __typename
+                id
+                images {
+                  url
                 }
-                `,
+              }
+            }
+          }
+        }
+      `,
         });
     }
     async getPrompt(promptId) {
-        return await this.client
-            .query({
+        return await this.client.query({
             query: (0, client_1.gql) `
-                query {
-                    requestedPrompts(limit: 100, orderBy:{orderBy:CreatedAt, direction: Dsc}) {
-                    status
-                    createdAt
-                    result {
-                        ... on Result_StableDiffusionV1_4 {
-                        __typename
-                        id
-                        images {
-                            url
-                        }
-                        }
-                    }
-                    }
+        query {
+          requestedPrompts(
+            limit: 100
+            orderBy: { orderBy: CreatedAt, direction: Dsc }
+          ) {
+            status
+            createdAt
+            result {
+              ... on Result_StableDiffusionV1_4 {
+                __typename
+                id
+                images {
+                  url
                 }
-                `,
+              }
+            }
+          }
+        }
+      `,
         });
     }
     async getPromptResult(promptId) {
-        const res = await this.client
-            .query({
+        const res = await this.client.query({
             query: (0, client_1.gql) `
-                query Prompt($uuid: String!) {
-                    requestedPrompt(uuid: $uuid) {
-                    status
-                    result {
-                        ... on Result_StableDiffusionV1_4 {
-                            __typename
-                            images {
-                                url
-                            }
-                        }
-                    }
-                    }
-                }`,
-            variables: { uuid: promptId }
+        query Prompt($uuid: String!) {
+          requestedPrompt(uuid: $uuid) {
+            status
+            result {
+              ... on Result_StableDiffusionV1_4 {
+                __typename
+                images {
+                  url
+                }
+              }
+            }
+          }
+        }
+      `,
+            variables: { uuid: promptId },
         });
         return res;
     }
     async getPromptStatus(promptId) {
-        const res = await this.client
-            .query({
+        const res = await this.client.query({
             query: (0, client_1.gql) `
-                query Prompt($uuid: String!) {
-                    requestedPrompt(uuid: $uuid) {
-                    status
-                    }
-                }`,
+        query Prompt($uuid: String!) {
+          requestedPrompt(uuid: $uuid) {
+            status
+          }
+        }
+      `,
             variables: { uuid: promptId },
-            fetchPolicy: "no-cache"
+            fetchPolicy: "no-cache",
         });
         return res.data.requestedPrompt.status;
     }
+    async waitOnResult(promptId, timeout = 10000, delay = 100) {
+        const promptResult = this.updateCache.statusChanges[promptId];
+        if (promptResult !== undefined) {
+            return promptResult;
+        }
+        const status = await this.getPromptStatus(promptId);
+        if (status.toLowerCase() === "completed") {
+            return apolloToCacheResult(await this.getPromptResult(promptId));
+        }
+        while (timeout > 0) {
+            const promptResult = this.updateCache.statusChanges[promptId];
+            if (promptResult !== undefined) {
+                return promptResult;
+            }
+            await new Promise((f) => setTimeout(f, delay));
+            timeout -= delay;
+        }
+        return { data: null, error: "timeout" };
+    }
     async requestNewPrompt(prompt) {
-        return await this.client
-            .mutate({
+        return await this.client.mutate({
             mutation: (0, client_1.gql) `
-                mutation Prompt($prompt: String!) {
-                    requestNewPrompt(
-                        body: {
-                        stableDiffusionV1_4: {
-                            action: txt2img
-                            prompt: $prompt
-                        }
-                        }
-                    ) {
-                        id
-                        prompt
-                    }
-                    }
-                `,
-            variables: { prompt }
+        mutation Prompt($prompt: String!) {
+          requestNewPrompt(
+            body: { stableDiffusionV1_4: { action: txt2img, prompt: $prompt } }
+          ) {
+            id
+            prompt
+          }
+        }
+      `,
+            variables: { prompt },
         });
     }
 }
--- index.ts
+++ index.ts
@@ -1,126 +1,246 @@
-export type { PromptRequest } from './graphql';
-import { ApolloClient, ApolloQueryResult, gql, InMemoryCache } from "@apollo/client";
-import { PromptRequest } from "./graphql";
+export type { PromptRequest } from "./graphql";
+import {
+  ApolloClient,
+  ApolloQueryResult,
+  gql,
+  InMemoryCache,
+} from "@apollo/client";
+import Pusher from "pusher-js";
+import { PromptRequest, User } from "./graphql";
+
+export const DEFAULT_URI = "https://www.promptzero.com/api/graphql";
+
+interface PromptZeroConstructor {
+  apiKey: string;
+  uri: string;
+  realTime: boolean;
+}
+
+export async function initPromptZero(
+  apiKey: string,
+  uri = DEFAULT_URI,
+  realTime = true
+): Promise<PromptZero> {
+  const p = new PromptZero({ apiKey, uri, realTime });
+  await p.init();
+  return p;
+}
+
+export interface CacheResultSuccess {
+  data: PromptRequest;
+  error: null;
+}
+export interface CacheResultError {
+  data: null;
+  error: string;
+}
+
+export type CacheResult = CacheResultError | CacheResultSuccess;
+
+interface PromptZeroListenCache {
+  statusChanges: Record<string, CacheResult>;
+}
+
+function apolloToCacheResult(
+  result: ApolloQueryResult<{
+    requestedPrompt: PromptRequest;
+  }>
+) {
+  const error = result.error;
+  if (error === undefined) {
+    return {
+      error: null,
+      data: result.data.requestedPrompt,
+    };
+  } else {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
 export class PromptZero {
-    client: ApolloClient<any>;
-    
-    constructor(apiKey: string, uri: string = "https://www.promptzero.com/api/graphql") {
-        this.client = new ApolloClient({
-            cache: new InMemoryCache(),
-            headers: {
-                authorization: apiKey,
-            },
-            uri
-            });
-    }
+  client: ApolloClient<any>;
+  pusher: Pusher;
+  userId: string = "";
+  updateCache: PromptZeroListenCache = { statusChanges: {} };
 
-    async getPrompts(): Promise<ApolloQueryResult<{requestedPrompts: PromptRequest[]}>> {
-        return await this.client
-            .query({
-                query: gql`
-                query {
-                    requestedPrompts(limit: 100, orderBy:{orderBy:CreatedAt, direction: Dsc}) {
-                    status
-                    createdAt
-                    prompt
-                    result {
-                        ... on Result_StableDiffusionV1_4 {
-                        __typename
-                        id
-                        images {
-                            url
-                        }
-                        }
-                    }
-                    }
+  constructor({ apiKey, uri, realTime }: PromptZeroConstructor) {
+    this.pusher = new Pusher("8499475eff3faa817087", {
+      cluster: "us3",
+    });
+    this.pusher.signin();
+    this.client = new ApolloClient({
+      cache: new InMemoryCache(),
+      headers: {
+        authorization: apiKey,
+      },
+      uri,
+    });
+  }
+
+  async init() {
+    this.userId = (await this.getUser()).data.user.id;
+    const channel = this.pusher.subscribe(this.userId);
+    channel.bind("status-change", (promptId: string) =>
+      this.getPromptResult(promptId)
+        .then((result) => {
+          this.updateCache.statusChanges[promptId] =
+            apolloToCacheResult(result);
+        })
+        .catch((e) => {
+          this.updateCache.statusChanges[promptId] = {
+            error: e.message,
+            data: null,
+          };
+        })
+    );
+  }
+
+  async getUser(): Promise<ApolloQueryResult<{ user: User }>> {
+    return await this.client.query({
+      query: gql`
+        query {
+          user {
+            id
+          }
+        }
+      `,
+    });
+  }
+
+  async getPrompts(): Promise<
+    ApolloQueryResult<{ requestedPrompts: PromptRequest[] }>
+  > {
+    return await this.client.query({
+      query: gql`
+        query {
+          requestedPrompts(
+            limit: 100
+            orderBy: { orderBy: CreatedAt, direction: Dsc }
+          ) {
+            status
+            createdAt
+            prompt
+            result {
+              ... on Result_StableDiffusionV1_4 {
+                __typename
+                id
+                images {
+                  url
                 }
-                `,
-            });    
-    }
+              }
+            }
+          }
+        }
+      `,
+    });
+  }
 
-    async getPrompt(promptId: string): Promise<ApolloQueryResult<any>> {
-        return await this.client
-            .query({
-                query: gql`
-                query {
-                    requestedPrompts(limit: 100, orderBy:{orderBy:CreatedAt, direction: Dsc}) {
-                    status
-                    createdAt
-                    result {
-                        ... on Result_StableDiffusionV1_4 {
-                        __typename
-                        id
-                        images {
-                            url
-                        }
-                        }
-                    }
-                    }
+  async getPrompt(promptId: string): Promise<ApolloQueryResult<any>> {
+    return await this.client.query({
+      query: gql`
+        query {
+          requestedPrompts(
+            limit: 100
+            orderBy: { orderBy: CreatedAt, direction: Dsc }
+          ) {
+            status
+            createdAt
+            result {
+              ... on Result_StableDiffusionV1_4 {
+                __typename
+                id
+                images {
+                  url
                 }
-                `,
-            });    
+              }
+            }
+          }
+        }
+      `,
+    });
+  }
+
+  async getPromptResult(
+    promptId: string
+  ): Promise<ApolloQueryResult<{ requestedPrompt: PromptRequest }>> {
+    const res = await this.client.query({
+      query: gql`
+        query Prompt($uuid: String!) {
+          requestedPrompt(uuid: $uuid) {
+            status
+            result {
+              ... on Result_StableDiffusionV1_4 {
+                __typename
+                images {
+                  url
+                }
+              }
+            }
+          }
+        }
+      `,
+      variables: { uuid: promptId },
+    });
+
+    return res;
+  }
+
+  async getPromptStatus(promptId: string): Promise<string> {
+    const res = await this.client.query({
+      query: gql`
+        query Prompt($uuid: String!) {
+          requestedPrompt(uuid: $uuid) {
+            status
+          }
+        }
+      `,
+      variables: { uuid: promptId },
+      fetchPolicy: "no-cache",
+    });
+
+    return res.data.requestedPrompt.status;
+  }
+
+  async waitOnResult(
+    promptId: string,
+    timeout: number = 10_000,
+    delay: number = 100
+  ): Promise<CacheResult> {
+    const promptResult = this.updateCache.statusChanges[promptId];
+    if (promptResult !== undefined) {
+      return promptResult;
     }
-
-    async getPromptResult(promptId: string): Promise<ApolloQueryResult<any>> {
-        const res = await this.client
-            .query({
-                query: gql`
-                query Prompt($uuid: String!) {
-                    requestedPrompt(uuid: $uuid) {
-                    status
-                    result {
-                        ... on Result_StableDiffusionV1_4 {
-                            __typename
-                            images {
-                                url
-                            }
-                        }
-                    }
-                    }
-                }`,
-                variables: {uuid: promptId} 
-            });
-
-        return res;
+    const status = await this.getPromptStatus(promptId);
+    if (status.toLowerCase() === "completed") {
+      return apolloToCacheResult(await this.getPromptResult(promptId));
     }
-
-    async getPromptStatus(promptId: string): Promise<string> {
-        const res = await this.client
-            .query({
-                query: gql`
-                query Prompt($uuid: String!) {
-                    requestedPrompt(uuid: $uuid) {
-                    status
-                    }
-                }`,
-                variables: {uuid: promptId},
-                fetchPolicy: "no-cache" 
-            });
-
-        return res.data.requestedPrompt.status;
+    while (timeout > 0) {
+      const promptResult = this.updateCache.statusChanges[promptId];
+      if (promptResult !== undefined) {
+        return promptResult;
+      }
+      await new Promise((f) => setTimeout(f, delay));
+      timeout -= delay;
     }
+    return { data: null, error: "timeout" };
+  }
 
-    async requestNewPrompt(prompt: string): Promise<any> {
-        return await this.client
-            .mutate({
-                mutation: gql`
-                mutation Prompt($prompt: String!) {
-                    requestNewPrompt(
-                        body: {
-                        stableDiffusionV1_4: {
-                            action: txt2img
-                            prompt: $prompt
-                        }
-                        }
-                    ) {
-                        id
-                        prompt
-                    }
-                    }
-                `,
-                variables: {prompt}
-            });    
-    }
-
-    
+  async requestNewPrompt(prompt: string): Promise<any> {
+    return await this.client.mutate({
+      mutation: gql`
+        mutation Prompt($prompt: String!) {
+          requestNewPrompt(
+            body: { stableDiffusionV1_4: { action: txt2img, prompt: $prompt } }
+          ) {
+            id
+            prompt
+          }
+        }
+      `,
+      variables: { prompt },
+    });
+  }
 }
